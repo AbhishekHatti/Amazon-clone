9d167d57c1643bfaa3fdc1b975d4918167fe68f2
AbhishekGHatti <abhi9663295215@gmail.com>
2020-08-15 14:20:11 +0530

    amazon clone app

---
+++ src/components/Checkout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+function Checkout() {
+  return (
+    <div>
+      <Header />
+      <h1>Checkout page </h1>
+    </div>
+  );
+}
+
+export default Checkout;
